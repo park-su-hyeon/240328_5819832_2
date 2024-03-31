fb7ff59f4ff9fb41da84a3729fd83bc748652607
suhyeon <suhyeon3684@naver.com>
2024-03-31 15:30:16 +0900

    초기 커밋.

---
+++ 240328_5819832_2.c
@@ -0,0 +1,51 @@
+﻿#include <stdio.h>
+#define MAX_DEGREE 101
+
+typedef struct {
+    int degree;
+    float coef[MAX_DEGREE];
+} polynomial;
+
+void print_poly(polynomial p);
+
+polynomial poly_mult(polynomial a, polynomial b) {
+    polynomial result;
+    int i, j;
+
+
+    result.degree = a.degree + b.degree;
+
+
+    for (i = 0; i <= result.degree; i++)
+        result.coef[i] = 0;
+
+
+    for (i = 0; i <= a.degree; i++) {
+        for (j = 0; j <= b.degree; j++) {
+            result.coef[i + j] += a.coef[i] * b.coef[j];
+        }
+    }
+
+    return result;
+}
+
+int main() {
+    polynomial a = { 3, {4, 3, 2, 1} };
+    polynomial b = { 2, {3, 2, 8} };
+    polynomial c;
+
+    print_poly(a);
+    print_poly(b);
+
+    c = poly_mult(a, b);
+    printf("--------------------------------\n");
+    print_poly(c);
+
+    return 0;
+}
+
+void print_poly(polynomial p) {
+    for (int i = p.degree; i > 0; i--)
+        printf("%3.1fx^%d + ", p.coef[p.degree - i], i);
+    printf("%3.1f \n", p.coef[p.degree]);
+}
